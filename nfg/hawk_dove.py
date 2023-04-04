f090e5aa82b861243c99f9aa6b3c9721fcdf7e2f
Willem Röpke <willem.ropke@vub.be>
2023-04-04 11:38:46 +0200

    Implement the Hawk-Dove game

---
+++ nfg/hawk_dove.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+
+def hawk_dove(batch_size=1, min_r=0, max_r=5, rng=None, seed=None):
+    """Creates a hawk-dove game.
+
+    This generates a matrix with payoffs
+
+    b, b d, a
+    a, d c, c
+
+    Where a > b > c > d
+
+    Note:
+        This is described incorrectly in the GAMUT documentation as well as source-code comments. The games are in fact
+        generated using the above payoff scheme.
+
+    Args:
+        batch_size (int, optional): The batch size. Defaults to 1.
+        min_r (int, optional): The minimum reward. Defaults to 0.
+        max_r (int, optional): The maximum reward. Defaults to 5.
+        rng (np.random.Generator, optional): The random number generator. Defaults to None.
+        seed (int, optional): The random seed. Defaults to None.
+
+    Returns:
+        ndarray: A hawk-dove game.
+    """
+    rng = rng if rng is not None else np.random.default_rng(seed)
+    min_diff = 1e-4
+
+    # Sequentially generate the a, b, c and d payoffs. The minimum payoffs for the largest payoffs are raised to ensure
+    # a strictly decreasing sequence.
+    a = rng.uniform(low=min_r + 3 * min_diff, high=max_r, size=batch_size)
+    b = rng.uniform(low=min_r + 2 * min_diff, high=a, size=batch_size)
+    c = rng.uniform(low=min_r + min_diff, high=b, size=batch_size)
+    d = rng.uniform(low=min_r, high=c, size=batch_size)
+
+    payoff_matrices = np.zeros((batch_size, 2, 2, 2))
+    payoff_matrices[:, [0, 1], [1, 0], [0, 1]] = a.reshape(batch_size, -1)
+    payoff_matrices[:, [0, 1], [0, 0], [0, 0]] = b.reshape(batch_size, -1)
+    payoff_matrices[:, [0, 1], [1, 1], [1, 1]] = c.reshape(batch_size, -1)
+    payoff_matrices[:, [0, 1], [0, 1], [1, 0]] = d.reshape(batch_size, -1)
+
+    if batch_size == 1:
+        return payoff_matrices[0]
+    else:
+        return payoff_matrices
