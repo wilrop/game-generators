55c16309a4d6983882235a693e276a64b2fc35c7
Willem Röpke <willem.ropke@vub.be>
2023-04-17 16:22:37 +0200

    Implement the identity game generator

---
+++ multi_objective/identity.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def identity(num_players, num_actions):
+    """Generate an identity game.
+
+    Note:
+        An identity game is defined as a game where the vector payoff is the joint strategy.
+
+    Args:
+        num_players (int): The number of players.
+        num_actions (int): The number of actions.
+
+    Returns:
+        np.ndarray: An identity game.
+    """
+    dim = num_actions * num_players
+    strats = np.eye(num_actions)
+
+    identity_game = np.zeros((num_players, *([num_actions] * num_players), dim))
+    for idx in np.ndindex(*([num_actions] * num_players)):
+        payoff = np.zeros(dim)
+        for i, s in enumerate(idx):
+            payoff[i * num_actions:(i + 1) * num_actions] = strats[s]
+        identity_game[(slice(None),) + idx] = payoff
+    return identity_game
