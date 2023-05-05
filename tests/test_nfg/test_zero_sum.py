a1ed3ab2bb497990a72eecc42a43192184b55de8
Willem Röpke <willem.ropke@vub.be>
2023-05-05 14:58:30 +0200

    Start implementation of tests

---
+++ tests/test_nfg/test_zero_sum.py
@@ -0,0 +1,26 @@
+import unittest
+import numpy as np
+from gage.nfg.zero_sum import zero_sum
+
+
+class TestBestResponse(unittest.TestCase):
+    n_players = 3
+    num_actions = 10
+    batch_size = 100
+    seed = 0
+
+    def check_sum(self, payoff_matrices):
+        summed = np.sum(payoff_matrices, axis=1)
+        np.testing.assert_almost_equal(summed, np.zeros_like(summed))
+
+    def test_2p(self):
+        payoff_matrices = zero_sum(2, self.num_actions, batch_size=self.batch_size, seed=self.seed)
+        self.check_sum(payoff_matrices)
+
+    def test_np(self):
+        payoff_matrices = zero_sum(self.n_players, self.num_actions, batch_size=self.batch_size, seed=self.seed)
+        self.check_sum(payoff_matrices)
+
+
+if __name__ == '__main__':
+    unittest.main()
