f63158ecb182cb6993c0223fc8bad3aa3b100f29
Willem Röpke <willem.ropke@vub.be>
2023-05-05 17:18:49 +0200

    Bugfix covariant games to output correctly ordered payoffs

--- gage/nfg/covariant.py
+++ gage/nfg/covariant.py
@@ -11,8 +11,8 @@ def covariant(num_players, num_actions, batch_size=1, mean=0, std=1, cov=0, rng=
         num_players (int): The number of players.
         num_actions (int): The number of actions.
         batch_size (int, optional): The batch size. Defaults to 1.
-        mean (float, optional): The mean of the normal distribution. Defaults to 0.
-        std (float, optional): The standard deviation of the normal distribution. Defaults to 1.
+        mean (float or array_like, optional): The mean of the normal distribution. Defaults to 0.
+        std (float or array_like, optional): The standard deviation of the normal distribution. Defaults to 1.
         cov (float, optional): The covariance of the normal distribution. Defaults to 0.
         rng (np.random.Generator, optional): The random number generator. Defaults to None.
         seed (int, optional): The random seed. Defaults to None.
@@ -24,13 +24,13 @@ def covariant(num_players, num_actions, batch_size=1, mean=0, std=1, cov=0, rng=
 
     if type(mean) == int or type(mean) == float:
         mean = np.full(num_players, mean)
-        cov = np.full((num_players, num_players), cov)
-        np.fill_diagonal(cov, std)
+
+    cov = np.full((num_players, num_players), cov)
+    np.fill_diagonal(cov, std)
 
     num_payoffs = (num_actions ** num_players) * batch_size
     player_payoffs = rng.multivariate_normal(mean, cov, size=num_payoffs)
-    payoff_matrices = np.expand_dims(player_payoffs, -1).swapaxes(0, 1).reshape(
-        (batch_size, num_players, *([num_actions] * num_players)))
+    payoff_matrices = player_payoffs.T.reshape(num_players, batch_size, *([num_actions] * num_players)).swapaxes(0, 1)
 
     if batch_size == 1:
         return payoff_matrices[0]
