2e7c9844ef4555c3a1bf9919ff404918cc9f2951
Willem Röpke <willem.ropke@vub.be>
2023-05-08 20:21:53 +0200

    Implement war of attrition and tests for it

---
+++ gage/nfg/war_of_attrition.py
@@ -0,0 +1,42 @@
+import numpy as np
+from gage.utils.payoffs import strictly_decreasing
+
+
+def war_of_attrition(timesteps, batch_size=1, min_r=20, max_r=20, min_dec=1, max_dec=3, rng=None, seed=None):
+    """Create a game of grab the dollar.
+
+    This generates a matrix with payoffs a > b > c such that when two players pick the same time they receive c and
+    otherwise the player with the lowest time receives a and the other receives b.
+
+    Args:
+        timesteps (int): The number of timesteps.
+        batch_size (int, optional): The batch size. Defaults to 1.
+        min_r (int, optional): The minimum reward. Defaults to 0.
+        max_r (int, optional): The maximum reward. Defaults to 5.
+        min_dec (int, optional): The minimum decrement. Defaults to 1.
+        max_dec (int, optional): The maximum decrement. Defaults to 1.
+        rng (np.random.Generator, optional): The random number generator. Defaults to None.
+        seed (int, optional): The random seed. Defaults to None.
+
+    Returns:
+        np.ndarray: A game of grab the dollar.
+    """
+    rng = rng if rng is not None else np.random.default_rng(seed)
+
+    valuations = rng.uniform(low=min_r, high=max_r, size=(batch_size, 2, 1, 1)).repeat(timesteps, axis=2).repeat(
+        timesteps, axis=3)
+    decrements = rng.uniform(low=min_dec, high=max_dec, size=(batch_size, 2))
+
+    u1, u2 = np.triu_indices(timesteps, k=1)
+    l1, l2 = np.tril_indices(timesteps, k=-1)
+    d1, d2 = np.diag_indices(timesteps)
+
+    multipliers = np.tile(np.arange(timesteps), (timesteps, 1))
+    min_matrix = np.minimum(multipliers, multipliers.T)
+    decrements = decrements[:, :, None, None] * min_matrix[None, :, :]
+
+    valuations[:, 0, u1, u2] = 0
+    valuations[:, 1, l1, l2] = 0
+    valuations[:, :, d1, d2] /= 2
+    valuations -= decrements
+    return valuations
---
+++ tests/test_nfg/test_war_of_attrition.py
@@ -0,0 +1,58 @@
+import unittest
+import numpy as np
+from gage.nfg.war_of_attrition import war_of_attrition
+
+
+class TestWarOfAttrition(unittest.TestCase):
+    batch_size = 100
+    min_r = 10
+    max_r = 20
+    min_dec = 1
+    max_dec = 3
+    seed = 0
+
+    def check_war_of_attrition(self, timesteps, payoff_matrices):
+        for payoffs in payoff_matrices:
+            m1, m2 = payoffs
+            v1 = m1[1, 0]
+            v2 = m2[0, 1]
+            dec1 = v1 - m1[2, 1]
+            dec2 = v2 - m2[1, 2]
+
+            for idx in np.ndindex(timesteps, timesteps):
+                a1, a2 = idx
+                if a1 == a2:
+                    np.testing.assert_almost_equal(m1[a1, a2], (v1 / 2) - a1 * dec1)
+                    np.testing.assert_almost_equal(m2[a1, a2], (v2 / 2) - a2 * dec2)
+                elif a1 > a2:
+                    np.testing.assert_almost_equal(m1[a1, a2], v1 - a2 * dec1)
+                    np.testing.assert_almost_equal(m2[a1, a2], 0 - a2 * dec2)
+                else:
+                    np.testing.assert_almost_equal(m1[a1, a2], 0 - a1 * dec1)
+                    np.testing.assert_almost_equal(m2[a1, a2], v2 - a1 * dec2)
+
+    def test_small(self):
+        timesteps = 5
+        payoff_matrices = war_of_attrition(timesteps,
+                                           batch_size=self.batch_size,
+                                           min_r=self.min_r,
+                                           max_r=self.max_r,
+                                           min_dec=self.min_dec,
+                                           max_dec=self.max_dec,
+                                           seed=self.seed)
+        self.check_war_of_attrition(timesteps, payoff_matrices)
+
+    def test_large(self):
+        timesteps = 20
+        payoff_matrices = war_of_attrition(timesteps,
+                                           batch_size=self.batch_size,
+                                           min_r=self.min_r,
+                                           max_r=self.max_r,
+                                           min_dec=self.min_dec,
+                                           max_dec=self.max_dec,
+                                           seed=self.seed)
+        self.check_war_of_attrition(timesteps, payoff_matrices)
+
+
+if __name__ == '__main__':
+    unittest.main()
