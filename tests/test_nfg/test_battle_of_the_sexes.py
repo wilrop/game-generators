946fbf36fddbe36a804921d6b3dc7d6dd08a95a0
Willem Röpke <willem.ropke@vub.be>
2023-05-09 10:44:15 +0200

    Add test for battle of the sexes game

---
+++ tests/test_nfg/test_battle_of_the_sexes.py
@@ -0,0 +1,39 @@
+import unittest
+import numpy as np
+from gage.nfg.battle_of_the_sexes import battle_of_the_sexes
+
+
+class TestBattleOfTheSexes(unittest.TestCase):
+    batch_size = 100
+    min_r = 10
+    max_r = 20
+    seed = 0
+
+    def test_battle_of_the_sexes(self):
+        payoff_matrices = battle_of_the_sexes(batch_size=self.batch_size,
+                                              min_r=self.min_r,
+                                              max_r=self.max_r,
+                                              seed=self.seed)
+        for payoffs in payoff_matrices:
+            m1, m2 = payoffs
+            if m1[0, 0] == m2[0, 0]:
+                self.assertTrue(m1[0, 0] == m1[1, 1] == m2[1, 1])
+                c = m1[0, 0]
+                p1 = m1[0, 1]
+                p2 = m1[1, 0]
+                p3 = m2[0, 1]
+                p4 = m2[1, 0]
+            else:
+                self.assertTrue(m1[0, 1] == m2[0, 1] == m1[1, 0] == m2[1, 0])
+                c = m1[0, 1]
+                p1 = m1[0, 0]
+                p2 = m1[1, 1]
+                p3 = m2[0, 0]
+                p4 = m2[1, 1]
+
+            self.assertTrue(p1 == p4 and p2 == p3)
+            self.assertTrue(c < p1 < p2 or c < p2 < p1)
+
+
+if __name__ == '__main__':
+    unittest.main()
