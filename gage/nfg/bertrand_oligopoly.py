63920d2ba333771fbdb5bf7e3c6ccf5ff43180b2
Willem Röpke <willem.ropke@vub.be>
2023-11-06 12:13:50 +0100

    Implement Bertrand oligopoly game

--- gage/nfg/bertrand_oligopoly.py
+++ gage/nfg/bertrand_oligopoly.py
@@ -3,8 +3,8 @@ import numpy as np
 
 def bertrand_oligopoly(num_players,
                        num_actions,
-                       cost_func,
-                       demand_func,
+                       cost_fun,
+                       demand_fun,
                        batch_size=1):
     """Creates an instance of a Bertrand Oligopoly using arbitrary cost and demand functions.
 
@@ -18,6 +18,8 @@ def bertrand_oligopoly(num_players,
     Args:
         num_players (int): The number of players.
         num_actions (int): The number of actions.
+        cost_fun (callable): The cost function.
+        demand_fun (callable): The demand function.
         batch_size (int, optional): The batch size. Defaults to 1.
 
     Returns:
@@ -27,11 +29,11 @@ def bertrand_oligopoly(num_players,
     costs = {}
 
     for price in range(1, num_actions + 1):
-        demands[price] = demand_func(price, batch_size=batch_size)
+        demands[price] = demand_fun([price])
         costs[price] = {}
 
         for m in range(1, num_players + 1):
-            costs[price][m] = cost_func(demands[price] / m, batch_size=batch_size)
+            costs[price][m] = cost_fun(demands[price] / m)
 
     payoff_matrices = np.zeros((batch_size, num_players, *([num_actions] * num_players)))
 
@@ -39,7 +41,8 @@ def bertrand_oligopoly(num_players,
         min_players = np.nonzero(idx == np.min(idx))[0]
         m = len(min_players)
         min_price = np.min(idx) + 1
-        payoff_matrices[(slice(None), min_players) + idx] = min_price * (demands[min_price] / m) - costs[min_price][m]
+        payoffs = min_price * (demands[min_price] / m) - costs[min_price][m]
+        payoff_matrices[(slice(None), min_players) + idx] = payoffs
 
     if batch_size == 1:
         return payoff_matrices[0]
