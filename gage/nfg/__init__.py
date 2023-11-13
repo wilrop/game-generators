5d277aed0c86fe9a3fe43858da83793210ba18b9
Willem Röpke <willem.ropke@vub.be>
2023-11-13 09:31:59 +0100

    Implement potential game

--- gage/nfg/__init__.py
+++ gage/nfg/__init__.py
@@ -4,10 +4,52 @@ from gage.nfg.chicken import chicken
 from gage.nfg.congestion import congestion
 from gage.nfg.covariant import covariant
 from gage.nfg.discrete_uniform import discrete_uniform
-from gage.nfg.generate_nfg import generate_nfg
 from gage.nfg.grab_the_dollar import grab_the_dollar
 from gage.nfg.hawk_dove import hawk_dove
 from gage.nfg.majority_voting import majority_voting
+from gage.nfg.potential import potential
 from gage.nfg.random_uniform import random_uniform
 from gage.nfg.war_of_attrition import war_of_attrition
 from gage.nfg.zero_sum import zero_sum
+
+
+def generate_nfg(game, *args, **kwargs):
+    """Generate a batch of normal-form games from a specified distribution.
+
+    Args:
+        game (str): The game to generate.
+        args: The arguments to pass to the generator.
+        kwargs: The keyword arguments to pass to the generator.
+
+
+    Returns:
+        np.ndarray: The batch of games.
+    """
+    if game == 'bach_stravinsky':
+        return bach_stravinsky(*args, **kwargs)
+    elif game == 'bertrand_oligopoly':
+        return bertrand_oligopoly(*args, **kwargs)
+    elif game == 'chicken':
+        return chicken(*args, **kwargs)
+    elif game == 'congestion':
+        return congestion(*args, **kwargs)
+    elif game == 'covariant':
+        return covariant(*args, **kwargs)
+    elif game == 'discrete_uniform':
+        return discrete_uniform(*args, **kwargs)
+    elif game == 'grab_the_dollar':
+        return grab_the_dollar(*args, **kwargs)
+    elif game == 'hawk_dove':
+        return hawk_dove(*args, **kwargs)
+    elif game == 'majority_voting':
+        return majority_voting(*args, **kwargs)
+    elif game == 'potential':
+        return potential(*args, **kwargs)
+    elif game == 'random_uniform':
+        return random_uniform(*args, **kwargs)
+    elif game == 'war_of_attrition':
+        return war_of_attrition(*args, **kwargs)
+    elif game == 'zero_sum':
+        return zero_sum(*args, **kwargs)
+    else:
+        raise ValueError(f'Unknown game {game}')
---
+++ gage/nfg/potential.py
@@ -0,0 +1,77 @@
+import numpy as np
+from gage.utils.transforms import coordinate_grid, make_batched
+
+
+def potential(num_players,
+              num_actions,
+              potential_func,
+              batch_size=1,
+              min_r=0,
+              max_r=5,
+              weights=None,
+              rng=None,
+              seed=None):
+    """Create a potential game with a weighted potential function.
+
+    Note:
+        A pure strategy equilibrium always exists in a potential game.
+
+    Note:
+        The potential function should allow for broadcasting.
+
+    Args:
+        num_players (int): The number of players.
+        num_actions (int): The number of actions.
+        potential_func (callable): The potential function.
+        batch_size (int, optional): The batch size. Defaults to 1.
+        min_r (int, optional): The minimum reward. This is not the global minimum reward, but rather the  minimum reward
+            before uniquely determining the other rewards using the potential function. Defaults to 0.
+        max_r (int, optional): The maximum reward. A similar warning as for min_r applies. Defaults to 5.
+        weights (np.ndarray, optional): The weights for each player. Defaults to None.
+        rng (np.random.Generator, optional): The random number generator. Defaults to None.
+        seed (int, optional): The random seed. Defaults to None.
+
+    Returns:
+        np.ndarray: A potential game.
+    """
+    rng = rng if rng is not None else np.random.default_rng(seed)
+    weights = np.ones((batch_size, num_players)) if weights is None else weights
+
+    action_shape = (num_actions,) * num_players
+    payoff_shape = (batch_size, num_players) + action_shape
+
+    payoff_matrices = np.zeros(shape=payoff_shape)
+    coordinates = make_batched(coordinate_grid(action_shape), batch_size)
+    potentials = potential_func(coordinates)
+
+    for player in range(num_players):
+        # Set random starting values for the payoff matrix.
+        starting_idcs = [slice(None)] * num_players
+        starting_idcs[player] = 0
+        init_idcs = (slice(None), player) + tuple(starting_idcs)
+        payoff_matrices[init_idcs] = rng.uniform(low=min_r, high=max_r, size=payoff_matrices[init_idcs].shape)
+        opp_strats = np.delete(action_shape, player)
+
+        for opp_idx in np.ndindex(*opp_strats):  # Calculate the remaining values using the potential function.
+            strat_idcs = list(opp_idx)
+            strat_idcs.insert(player, slice(None))  # Indices for the strategies uniquely determined by the potential.
+
+            potential_idx = (slice(None),) + tuple(strat_idcs)  # Indices for the potential.
+
+            comp_idx = list(opp_idx)
+            comp_idx.insert(player, slice(0, 1))  # Index for the comparison strategy.
+
+            payoff_comp_idx = (slice(None), player) + tuple(comp_idx)  # Index for the comparison payoff.
+            potential_comp_idx = (slice(None),) + tuple(comp_idx)  # Index for the comparison potential.
+            new_potentials = potentials[potential_idx]
+            comp_potentials = potentials[potential_comp_idx]
+            comp_payoffs = payoff_matrices[payoff_comp_idx]
+
+            new_ps = (new_potentials - comp_potentials) / weights[:, player:player + 1] + comp_payoffs
+            new_idcs = (slice(None), player) + tuple(strat_idcs)
+            payoff_matrices[new_idcs] = new_ps  # Set the payoffs.
+
+    if batch_size == 1:
+        return payoff_matrices[0]
+    else:
+        return payoff_matrices
