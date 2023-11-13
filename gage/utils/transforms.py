da9a79a8eabf5c2a5ec42982e575cfe4d9d3228b
Willem Röpke <willem.ropke@vub.be>
2023-11-13 09:30:08 +0100

    Add additional transformations

--- gage/utils/transforms.py
+++ gage/utils/transforms.py
@@ -1,6 +1,32 @@
 import numpy as np
 
 
+def coordinate_grid(shape):
+    """Create a coordinate grid.
+
+    Args:
+        shape (Tuple[int]): The shape of the grid.
+
+    Returns:
+        np.ndarray: The coordinate grid.
+    """
+    indices = np.expand_dims(np.indices(shape), axis=-1)
+    return np.concatenate(indices, axis=-1)
+
+
+def make_batched(arr, batch_size):
+    """Make an array batched.
+
+    Args:
+        arr (array_like): The array to batch.
+        batch_size (int): The batch size.
+
+    Returns:
+        np.ndarray: The batched array.
+    """
+    return np.repeat(np.expand_dims(arr, axis=0), batch_size, axis=0)
+
+
 def to_joint_payoff(games, batched=True):
     """Convert a batch of games to joint payoff format.
 
